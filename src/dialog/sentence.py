8a8a1bf2d6a1357f3bed777a68d161e8456303cb
Séverin Lemaignan <severin.lemaignan@laas.fr>
2010-05-21 12:01:49 +0200

    Rewritten the Sentence class to make a correct usage of __str__()

--- src/dialog/sentence.py
+++ src/dialog/sentence.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#SVN:rev202 + PythonTidy
+#SVN:rev202 + PythonTidy + rewrite 'toString' methods using Python __str__ + test cases
 
 class Sentence:
 
@@ -24,45 +24,17 @@ class Sentence:
         self.sn = sn
         self.sv = sv
 
-    def getString(self):
-
-        def printGroupNominal(name, group_nominals):
-            for nom in group_nominals:
-                print name + 'det:' + str(nom.det)
-                print name + 'noun:' + str(nom.noun)
-                print name + 'adj:' + str(nom.adj)
-                if nom.relative != None:
-                    print name + 'relative:'
-                    nom.relative.getString()
-                if nom.noun_cmpl != []:
-                    print name + 'noun_cmpl:'
-                    printGroupNominal(name + 'noun_cmpl:',
-                            nom.noun_cmpl)
-
-        def printVerbalGroup(name, verbal_group):
-            print name + 'vrb_main:' + str(verbal_group.vrb_main)
-            print name + 'vrb_tense:' + str(verbal_group.vrb_tense)
-            print name + 'advrb: ' + str(verbal_group.advrb)
-            print name + 'vrb_adv' + str(verbal_group.vrb_adv)
-            if verbal_group.d_obj != []:
-                print name + 'd_obj:'
-                printGroupNominal(name + 'd_obj:', verbal_group.d_obj)
-            if verbal_group.vrb_sub_sentence != None:
-                print name + 'vrb_sub_sentence:'
-                for vrb_sub_s in verbal_group.vrb_sub_sentence:
-                    vrb_sub_s.getString()
-
-            if verbal_group.i_cmpl != []:
-                print name + 'i_cmpl:'
-                for i in verbal_group.i_cmpl:
-                    print name + 'i_cmpl:' + str(i.prep)
-                    printGroupNominal(name + 'i_cmpl:', i.nominal_group)
-
-        print self.data_type, '\n', self.aim, '\n'
-        if self.sn != []:
-            printGroupNominal('sn:', self.sn)
-        if self.sv != None:
-            printVerbalGroup('sv:', self.sv)
+    def __str__(self):
+        res = self.data_type + '\n' + \
+                self.aim + '\n'
+        
+        if self.sn:
+            for s in self.sn:
+                res += 'sn:\n\t' + str(s).replace("\n", "\n\t") + "\n"
+                
+        res += (('sv:\n\t' + str(self.sv).replace("\n", "\n\t") + "\n") if self.sv else "")
+        
+        return res
 
 
 class Nominal_Group:
@@ -89,7 +61,21 @@ class Nominal_Group:
         self.adj = adj
         self.noun_cmpl = noun_cmpl
         self.relative = relative
+        
+    def __str__(self):
+        res =   'det:' + str(self.det) + "\n" + \
+                'noun:' + str(self.noun) + "\n" + \
+                'adj:' + str(self.adj) + "\n"
 
+        if self.noun_cmpl:
+            for s in self.noun_cmpl:
+                res += 'noun_cmpl:\n\t' + str(s).replace("\n", "\n\t") + "\n"
+        
+        if self.relative:
+            for s in self.relative:
+                res += 'relative:\n\t' + str(s).replace("\n", "\n\t") + "\n"
+        
+        return res
 
 class Indirect_Complement:
 
@@ -102,6 +88,16 @@ class Indirect_Complement:
     def __init__(self, prep, nominal_group):
         self.prep = prep
         self.nominal_group = nominal_group
+        
+    def __str__(self):
+        res = 'prep:' + str(self.prep) + '\n'
+        
+        if self.nominal_group:
+            for s in self.nominal_group:
+                res += 'sn:\n\t' + str(s).replace("\n", "\n\t") + "\n"
+        
+        return res
+
 
 
 class Verbal_Group:
@@ -138,6 +134,27 @@ class Verbal_Group:
         self.vrb_adv = vrb_adv
         self.state = state
         self.vrb_sub_sentence = vrb_sub_sentence
+    
+    def __str__(self):
+        res =   'vrb_main:' + str(self.vrb_main) + "\n" + \
+                'vrb_tense:' + str(self.vrb_tense) + "\n" + \
+                'advrb: ' + str(self.advrb) + "\n" + \
+                'vrb_adv' + str(self.vrb_adv) + "\n"
+                
+        if self.d_obj:
+            for cmpl in self.d_obj:
+                res += 'd_obj:\n\t' + str(cmpl).replace("\n", "\n\t") + "\n"
+        
+        if self.i_cmpl:
+            for cmpl in self.i_cmpl:
+                res += 'i_cmpl:\n\t' + str(cmpl).replace("\n", "\n\t") + "\n"
+        
+        if self.vrb_sub_sentence:
+            for vrb_sub_s in self.vrb_sub_sentence:
+                res += 'vrb_sub_sentence:\n\t' + str(vrb_sub_s).replace("\n", "\n\t") + "\n"
+
+        
+        return res
 
 
 class ObjectInteraction:
@@ -156,4 +173,63 @@ class ObjectInteraction:
         self.date = date
         self.time = time
 
+def unit_tests():
 
+    sentence1 = Sentence('w_question',
+                        'location',
+                        [Nominal_Group(['the'],  ['mother'],[],None, None)],
+                        Verbal_Group(['be'], None,'present simple',None, None,['today'], None, None, None))
+    
+    print("*********************************")
+    print(sentence1)
+    
+    sentence2 = Sentence('statement', 
+                        '', 
+                        [Nominal_Group([],["Jido"],[],[],None), Nominal_Group([],["Danny"],[],[],None)], 
+                        Verbal_Group(["want"],None, 'infinitive',[],[],[],[],'affirmative', None))
+    
+    print("*********************************")
+    print(sentence2)
+    
+    sentence3 = Sentence('statement', 
+                        '', 
+                        [Nominal_Group([],["Holmes"],[],[],None), Nominal_Group([],["Sherlock"],[],[],None)], 
+                        Verbal_Group(["want"],
+                                    Verbal_Group(["eat"],None, 'infinitive',[],[],[],[],'affirmative', None), 
+                                    'past simple',
+                                    [],
+                                    [],
+                                    [],
+                                    [],
+                                    'negative', 
+                                    None))
+    
+    print("*********************************")
+    print(sentence3)
+    
+    sentence4 = Sentence('statement',
+                        '',
+                        [Nominal_Group( ['the'],  
+                                        ['bottle'],
+                                        ['blue', 'gray'],
+                                        [Nominal_Group(['my'],  ['mother'],[],[], [sentence2]), Nominal_Group(['my'],  ['father'],[],[], None)], 
+                                        None)], 
+                        Verbal_Group(['know'], 
+                                    None,
+                                    'present simple',
+                                    [Nominal_Group(['the'],  ["land"],['old'],[], None), Nominal_Group(['the'],  ["brand"],['lazy'],[], None)],
+                                    [
+                                        Indirect_Complement(['in'], 
+                                                            [Nominal_Group(['the'],  ['garden'],['green'],[], None)]), 
+                                        Indirect_Complement(['to'], 
+                                                            [Nominal_Group(['the'],  ['car'],['red'],[], None)])
+                                    ],
+                                    ["slowly"], 
+                                    ["now"], 
+                                    "affirmative", 
+                                    [sentence3]))
+    print("*********************************")
+    print(sentence4)
+    
+if __name__ == '__main__':
+    unit_tests()
