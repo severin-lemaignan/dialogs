850c26ecd33f1cde8d4f6290e757e4d274ca2a79
Patrick Tsemengue Mbarga <ptsemeng@chico.laas.fr>
2010-06-24 15:36:57 +0200

    processing statement data_type sentence

--- src/dialog/interpretation/content_analysis.py
+++ src/dialog/interpretation/content_analysis.py
@@ -14,12 +14,22 @@ class ContentAnalyser:
         self.builder = StatementBuilder()
         
     def analyse(self, sentence, current_speaker):
+        """analyse an imperative data_type sentence"""
         if sentence.data_type == 'imperative':
             logging.debug("Processing the content of an imperative sentence")
             return self.process_order(sentence, current_speaker)
+
+	
+        """analyse a statement data_type sentence"""
+        if sentence.data_type == 'statement':
+            logging.debug("Processing the content of a statement type sentence")
+            return self.process_statement(sentence, current_speaker)
+	
+
+
             
     def process_order(self, sentence, current_speaker):
-        flags = ['order', '', '', '', '', '', []]
+        flags = ['order', '', '', '', '', '', [], []]
         stmts = self.builder.processOrderSentence(
                             sentence, 
                             current_speaker, 
@@ -29,6 +39,20 @@ class ContentAnalyser:
             logging.info(">> " + s)
         
         return stmts
+
+
+    def process_statement(self, sentence, current_speaker):
+        flags = ['inform', '', '', '', '', '', [], []]
+        stmts = self.builder.processSentence(
+                            sentence,
+                            current_speaker,
+                            flags)
+        logging.info("Generated statements: ")        
+        for s in stmts:
+            logging.info(">> " + s)
+        
+        return stmts
+    
 
 def unit_tests():
     """This function tests the main features of the class ContentAnalysis"""
