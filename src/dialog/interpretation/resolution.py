7baeb713ee781d05a776a264ae0aa01c08e26fe0
Patrick Tsemengue Mbarga <ptsemeng@chico.laas.fr>
2010-07-07 16:03:00 +0200

    Modified Resolution according to the new version of StatementBuilder

--- src/dialog/interpretation/resolution.py
+++ src/dialog/interpretation/resolution.py
@@ -5,8 +5,8 @@ import logging
 
 from dialog_exceptions import UnsufficientInputError, UnknownVerb
 from resources_manager import ResourcePool
-
-from statements_builder import StatementBuilder #for nominal group discrimination
+from pyoro import Oro #TODO: Remove this when Oro is shared with everyone
+from statements_builder import NominalGroupStatementBuilder #for nominal group discrimination
 from discrimination import Discrimination
 
 class Resolver:
@@ -22,7 +22,7 @@ class Resolver:
     
     """
     
-    def resolve_references(self, nominal_group, current_speaker, current_object):
+    def resolve_references(self, nominal_group, current_speaker, current_object, onto):
         
         if nominal_group._resolved: #already resolved: possible after asking human for more details.
             return nominal_group
@@ -30,8 +30,13 @@ class Resolver:
         if not nominal_group.noun:
             return nominal_group
         
-        #TODO: noun[0] is bad! noun should be changed to be a single value instead of an array
-        if current_speaker and nominal_group.noun[0].lower() in ['me', 'i']:
+        #In the following , if there are two nouns in the same sentence
+        #they will be split into two different nominal groups
+        if onto and [nominal_group.noun[0],"INSTANCE"] in onto:
+            nominal_group.id = nominal_group.noun[0]
+            nominal_group._resolved = True
+            
+        if current_speaker and nominal_group.noun[0].lower() in ['me','i']:
             logging.debug("Replaced \"me\" or \"I\" by \"" + current_speaker + "\"")
             nominal_group.id = current_speaker
             nominal_group._resolved = True
@@ -45,16 +50,23 @@ class Resolver:
             logging.debug("Replaced the anaphoric reference \"it\" by " + current_object)
             nominal_group.id = current_object
             nominal_group._resolved = True
-        
+
+
         return nominal_group
     
     def resolve_groups_references(self, array_sn, current_speaker, current_object):
         #TODO: We should start with resolved_sn filled with sentence.sn and replace
         # 'au fur et a mesure' to avoid re-resolve already resolved nominal groups
+        
+        #TODO: remove connection. Use a variable that will be set up for a single connection to ORO
+        oro = Oro("localhost", 6969)
+         
         resolved_sn = []
         for sn in array_sn:
-            resolved_sn.append(self.resolve_references(sn, current_speaker, current_object))
+            onto = oro.lookup(sn.noun[0])
+            resolved_sn.append(self.resolve_references(sn, current_speaker, current_object, onto))
 
+        oro.close()
         return resolved_sn
             
     def references_resolution(self, sentence, current_speaker, current_object):
@@ -85,8 +97,8 @@ class Resolver:
         if nominal_group._resolved: #already resolved: possible after asking human for more details.
             return nominal_group
             
-        logging.debug("Resolving \"" + str(nominal_group) + "\"")      
-        stmts = builder.processNominalGroup([nominal_group], '?concept',{0:'query'})        
+        logging.debug("Resolving \"" + str(nominal_group) + "\"")
+        stmts = builder.process_nominal_group(self, nominal_group, '?concept')    
         logging.debug("Trying to identify this concept in "+ current_speaker + "'s model:")
         for s in stmts:
             logging.debug(s)
@@ -103,34 +115,56 @@ class Resolver:
         
         return nominal_group
     
-    def resolve_groups_nouns(self, array_sn, current_speaker, discriminator, builder):
-        #TODO: We should start with resolved_sn filled with sentence.sn and replace
-        # 'au fur et a mesure' to avoid re-resolve already resolved nominal groups
+    def resolve_groups_nouns(self, nominal_groups, current_speaker, discriminator, builder):
         resolved_sn = []
-        for sn in array_sn:
-            resolved_sn.append(self.resolve_nouns(sn, current_speaker, discriminator, builder))
+        for ng in nominal_groups:
+            resolved_sn.append(self.resolve_nouns(ng, current_speaker, discriminator, builder))
+            
 
         return resolved_sn
         
     def noun_phrases_resolution(self, sentence, current_speaker):
+        #NominalGroupStatementBuilder
+        builder = NominalGroupStatementBuilder(None,current_speaker)
         
-        builder = StatementBuilder(current_speaker)
-        builder._sentence = sentence
-        
+        #Discrimination
         discriminator = Discrimination()
 
         if sentence.sn:
-            sentence.sn = self.resolve_groups_nouns(sentence.sn, current_speaker, discriminator, builder)
+            sentence.sn = self.resolve_groups_nouns(sentence.sn, 
+                                                    current_speaker,
+                                                    discriminator,
+                                                    builder)
+        """new version """
+        if sentence.sv:
+            for sv in sentence.sv:
+                if sv.d_obj:
+                    sv.d_obj = self.resolve_groups_nouns(sv.d_obj, 
+                                                         current_speaker,
+                                                         discriminator,
+                                                         builder)
+        
+                if sv.i_cmpl:
+                    resolved_i_cmpl = []
+                    for i_cmpl in sv.i_cmpl:
+                        i_cmpl = self.resolve_groups_nouns(sv.i_cmpl, 
+                                                           current_speaker,
+                                                           discriminator,
+                                                           builder)
+                        resolved_i_cmpl.append(i_cmpl)
+                    sentence.sv.i_cmpl = resolved_i_cmpl
+                        
         
         
-        """
+        """old version
         process d_obj 
         """
+        """
         if sentence.sv.d_obj:
-            """
-            process d_obj : case: the yellow banana is good. yellow_banana hasFeature good. Where good is in the sentence.sv.d_obj[0].adj
-            There is no noun in this  nominal group, so no resolve_groups_nouns here.
-            """
+        
+            #process d_obj : case: the yellow banana is good. yellow_banana hasFeature good. Where good is in the sentence.sv.d_obj[0].adj
+            #There is no noun in this  nominal group, so no resolve_groups_nouns here.
+        
             if sentence.sv.vrb_main == ["be"] and\
                      sentence.sv.d_obj[0].adj != [] and\
                      sentence.sv.d_obj[0].noun == [] and\
@@ -157,7 +191,9 @@ class Resolver:
             resolved_i_cmpl.append(i_cmpl)
         
         sentence.sv.i_cmpl = resolved_i_cmpl
-            
+        """
+        
+           
         return sentence
     
     
