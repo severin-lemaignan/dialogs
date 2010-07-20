9f7ec02549e6667b740aef81dfe601794747f250
Séverin Lemaignan <severin.lemaignan@laas.fr>
2010-07-20 14:33:01 +0200

    Some doc on the UnsufficientInputError exception

--- src/dialog/dialog_exceptions.py
+++ src/dialog/dialog_exceptions.py
@@ -7,6 +7,20 @@ class DialogError(Exception):
         return repr(self.value)
         
 class UnsufficientInputError(DialogError):
+    """ This exception is raised when discrimination routines need more 
+    information from the user to actually find out the ID of the concept being
+    resolved.
+    
+    It can take 3 kind of parameters:
+    - (recommended) ['SUCCESS'|'FAILURE', [Sentence]]
+            -> 'SUCCESS' if the discrimination could find candidates but need
+            more details, 'FAILURE' if no candidates have been found.
+            -> list of instances of the Sentence class to be verbalized, containing 
+            the question to be asked to the user
+    - Sentence -> a instance of the Sentence class to be verbalized, containing 
+        the question to be asked to the user
+    - String -> the text to be send to the human, in natural language
+    """
     def __init__(self, value):
         self.value = value
     def __str__(self):
