5ac532f91bb302734f7b91c312678afebccbcfa5
Mahdi Chouayakh <mchouaya@laas.fr>
2010-07-23 16:00:41 +0200

    add 'as' is processing

--- src/dialog/parsing/analyse_verbal_structure.py
+++ src/dialog/parsing/analyse_verbal_structure.py
@@ -31,7 +31,7 @@ Statement of lists
 """
 aux_list=['have', 'has', 'had', 'is', 'are', 'am', 'was', 'were', 'will']
 adv_list=['here','tonight', 'yesterday', 'tomorrow', 'today', 'now']
-proposal_list=['in', 'on', 'at', 'from', 'to', 'about', 'for', 'next', 'last', 'ago', 'with', 'by', 'behind','behind+to','next+to','in+front+of']
+proposal_list=['in', 'on', 'at', 'from', 'to', 'about', 'for', 'next', 'last', 'ago', 'with', 'by', 'behind','behind+to','next+to','in+front+of','as']
 rel_list=['which', 'who','that']
 sub_list=['while', 'but','where', 'when', 'if']
 pronoun_list=['you', 'I', 'we', 'he', 'she', 'me', 'it', 'he', 'they', 'yours', 'mine', 'him']
