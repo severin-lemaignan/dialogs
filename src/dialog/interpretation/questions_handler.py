9606cc09ed8a7e3f37a61e1a3bfdf30f5c22c5f1
Patrick Tsemengue <ptsemeng@laas.fr>
2010-07-26 16:37:01 +0200

    Updated What-Question

--- src/dialog/interpretation/questions_handler.py
+++ src/dialog/interpretation/questions_handler.py
@@ -13,7 +13,6 @@ from pyoro import OroServerError
 from dialog_exceptions import DialogError
 from dialog_exceptions import GrammaticalError
 
-
 class QuestionHandler:
 	
 	def __init__(self, current_speaker = None):
@@ -31,20 +30,15 @@ class QuestionHandler:
 	
 	
 	def process_sentence(self, sentence):
-		
-		if not sentence.resolved():
-			raise DialogError("Trying to process an unresolved sentence!")	
-		
 		self._sentence = sentence
-		
 		#StatementBuilder
 		builder = StatementBuilder(self._current_speaker)
 		self._statements = builder.process_sentence(self._sentence)
 		logging.info("statement from Statement Builder: " + str(self._statements))
 		
-		#Case the question is a y_n_question
+		#Case the question is a y_n_question : check the fact in the ontology
 		if sentence.data_type == 'yes_no_question':
-			self._statements = self._resolve_action_verb_reference(self._statements)
+			self._statements = self._set_situation_id(self._statements)
 			
 			try:
 				logging.debug("Checking on the ontology: check(" + str(self._statements) + ")")
@@ -52,23 +46,24 @@ class QuestionHandler:
 			except AttributeError: #the ontology server is not started of doesn't know the method
 				pass
 			
-		#Case the question is a w_question
+		#Case the question is a w_question : find the concept the concept that answers the question
 		if sentence.data_type == 'w_question':
-			self._statements = self._extend_statement_from_sentence_aim(self._statements)
 			self._statements =  self._remove_statements_with_no_unbound_tokens(self._statements)
+			self._statements = self._extend_statement_from_sentence_aim(self._statements)
 			
-			try:
-				logging.debug("Searching the ontology: find(?concept, " + str(self._statements) + ")")
-				self._answer = ResourcePool().ontology_server.find('?concept', self._statements)
-			except AttributeError: #the ontology server is not started of doesn't know the method
+			if self._statements:
+				try:
+					logging.debug("Searching the ontology: find(?concept, " + str(self._statements) + ")")
+					self._answer = ResourcePool().ontology_server.find('?concept', self._statements)
+				except AttributeError: #the ontology server is not started of doesn't know the method
+					pass
+			else:
 				pass
+
 		
 		return self._answer
 		
-	def _resolve_action_verb_reference(self, statements):
-		"""
-		TODO: doc!! I don't understand what this method is doing
-		"""
+	def _set_situation_id(self, statements):
 		stmts = []
 		try:
 			
@@ -101,10 +96,11 @@ class QuestionHandler:
 		for sn in self._sentence.sn:
 			for sv in self._sentence.sv:	
 				for verb in sv.vrb_main:
+					role = self.get_role_from_sentence_aim(verb)
 					if verb.lower() == 'be':
-						stmts.append(sn.id + self.get_role_from_sentence_aim(verb) +'?concept')
+						stmts.append(sn.id + ' '+ role + ' ?concept')
 					else:
-						stmts.append('?event' + self.get_role_from_sentence_aim(verb) +'?concept')
+						stmts.append('?event ' + role + ' ?concept')
 		return current_statements + stmts
 	
 	
@@ -137,19 +133,23 @@ class QuestionHandler:
 				   None:'receivedBy',
 				   'has_goal':'hasGoal'}
 		#Dictionary for sentence.aim = 'thing' 
-		dic_thing={'be':'rdf:type', 
-				   None:'involves',
+		dic_thing={	'be':'owl:sameAs',
+					None:'involves',
 				   'acts_on_object':'actsOnObject'}
 		
 		#Dictionary for sentence.aim = 'aim' 
 		dic_manner={'be':'?sub_feature'}
+		
 		
 		#TODO:With dictionary from resource pool
 		#dic_aim = resourcePool.Dictionary(dic_aim)
 		#Dictionary for all
 		dic_aim = {'thing':dic_thing,
 				   'place':dic_place,
-				   'manner':dic_manner}
+				   'manner':dic_manner,
+				   'color':	{'be':'hasColor'},
+				   'size':	{'be':'hasSize'},
+				   }
 		
 		if verb in has_goal:
 			role = dic_aim[self._sentence.aim]['has_goal']
@@ -163,7 +163,7 @@ class QuestionHandler:
 			role = dic_aim[self._sentence.aim][None]
 			
 		
-		return (' ' + role + ' ')
+		return role
 	
 	
 	"""			
@@ -225,6 +225,7 @@ class TestQuestionHandler(unittest.TestCase):
 					 'blue_cube rdf:type Cube',
 					 'blue_cube hasColor blue',
 					 'blue_cube isOn table1',
+					 
 					 'another_cube rdf:type Cube',
 					 'another_cube isOn shelf1',
 					 'another_cube belongsTo SPEAKER',
@@ -303,7 +304,7 @@ class TestQuestionHandler(unittest.TestCase):
 		sentence = Sentence("w_question", "thing", 
 	                         [Nominal_Group([],
 	                                        ['you'],
-	                                        [''],
+	                                        [],
 	                                        [],
 	                                        [])],                                         
 	                         [Verbal_Group(['see'],
@@ -342,11 +343,11 @@ class TestQuestionHandler(unittest.TestCase):
 	                                       [],
 	                                       'affirmative',
 	                                       [])])
-		statement_query = ['?concept hasColor blue']
+		statement_query = ['blue_cube owl:sameAs ?concept']
 		expected_result = ['blue_cube']		
 		self.process(sentence , statement_query, expected_result) 
-		
-		
+	
+
 	
 	def test_9_w_question_this(self):
 		print "\n*************  test_9_w_question_this ******************"
@@ -369,10 +370,75 @@ class TestQuestionHandler(unittest.TestCase):
 		statement_query = ['SPEAKER focusesOn ?concept']
 		expected_result = ['another_cube']		
 		self.process(sentence , statement_query, expected_result) 
+	
+	def test_10_w_question(self):
+		print "\n*************  test_10_w_question ******************"
+		print "what object is blue?"
+		sentence = Sentence("w_question", "object", 
+	                         [],                                         
+	                         [Verbal_Group(['be'],
+	                                       [],
+	                                       'present simple',
+	                                       [Nominal_Group([],
+														  [],
+														  ['blue'],
+														  [],
+														  [])],
+	                                       [],
+	                                       [],
+	                                       [],
+	                                       'affirmative',
+	                                       [])])
+		statement_query = ['?concept owl:sameAs blue_cube']
+		expected_result = ['blue_cube']		
+		self.process(sentence , statement_query, expected_result)
+	
+	def test_11_w_question(self):
+		print "\n*************  test_11_w_question ******************"
+		print "what size is this?"
+		sentence = Sentence("w_question", "size", 
+	                         [Nominal_Group(['this'],
+	                                        [],
+	                                        [],
+	                                        [],
+	                                        [])],                                         
+	                         [Verbal_Group(['be'],
+	                                       [],
+	                                       'present simple',
+	                                       [],
+	                                       [],
+	                                       [],
+	                                       [],
+	                                       'affirmative',
+	                                       [])])
+		statement_query = ['another_cube hasSize ?concept']
+		expected_result = ['small']		
+		self.process(sentence , statement_query, expected_result)
+	
+	def test_12_w_question(self):
+		print "\n*************  test_12_w_question ******************"
+		print "what color is the blue_cube?"
+		sentence = Sentence("w_question", "color", 
+	                         [Nominal_Group(['the'],
+	                                        ['blue_cube'],
+	                                        [],
+	                                        [],
+	                                        [])],                                         
+	                         [Verbal_Group(['be'],
+	                                       [],
+	                                       'present simple',
+	                                       [],
+	                                       [],
+	                                       [],
+	                                       [],
+	                                       'affirmative',
+	                                       [])])
+		statement_query = ['blue_cube hasColor ?concept']
+		expected_result = ['blue']		
+		self.process(sentence , statement_query, expected_result)
 		
 	
 	"""
-	
 	def test_4_y_n_question(self):
 		print "\n*************  test_4_y_n_question action verb******************"
 		print "Did you get the blue cube?"
@@ -480,11 +546,11 @@ class TestQuestionHandler(unittest.TestCase):
 		expected_result = False		
 		self.process(sentence , statement_query, expected_result) 
 	
-	"""
+	
 	
 	
 		
-	"""
+	
 	def test_9_w_question(self):
 		print "\n*************  test_9_w_question ******************"
 		print "How is my car?"
@@ -521,16 +587,95 @@ class TestQuestionHandler(unittest.TestCase):
 		self.assertEqual(res, expected_result)
 
 def dump_resolved(sentence, current_speaker, current_recipient = None):
-	resolver = Resolver()
-	#Resolution
-	sentence = resolver.references_resolution(sentence,
-                                              current_speaker, 
-                                              None)
+	def resolve_ng(ngs, builder):		
+		for ng in ngs:
+			#Statement for resolution
+			logging.info("Statements sended to Resolution for discrmination for this nominal group...")
+			print(ng)
+			builder.process_nominal_group(ng, '?concept')
+			stmts = builder.get_statements()
+			builder.clear_statements()
+			for s in stmts:
+				logging.info("\t>>" + s)
+				
+			logging.info("--------------<<\n")
+			
+			#Dump resolution for StatementBuilder test ONLY
+			logging.info("Dump resolution for Question Handler test ONLY ...")
+			
+			resolved = True
+							
+			if ng._resolved:
+				pass
+			#personal pronoun
+			elif ng.noun in [['me'], ['Me'],['I']]:
+				ng.id = current_speaker
+			elif ng.noun in [['you'], ['You']]:
+				ng.id = current_recipient					   
+			
+			#Existing ID from the onotlogy
+			elif ng.noun:				
+				onto = ''
+				try:
+					onto =  ResourcePool().ontology_server.lookup(ng.noun[0])
+				except AttributeError: #the ontology server is not started of doesn't know the method
+					pass
+				
+					
+				if onto and [ng.noun[0], 'INSTANCE'] in onto:
+					ng.id = ng.noun[0]
+				
+			#Find Existing ID from the onotlogy
+			if not ng.id and stmts:
+				onto = ''
+				try:
+					onto =  ResourcePool().ontology_server.find('?concept', stmts)
+				except AttributeError: #the ontology server is not started of doesn't know the method
+					pass
+				
+					
+				if onto:
+					logging.info("\tFound in the Ontology " + str(onto) + " matching the statements send to resolution")
+					ng.id = onto[0]
 	
-	sentence = resolver.noun_phrases_resolution(sentence,
-											    current_speaker)
-	sentence = resolver.verbal_phrases_resolution(sentence)
+			#Nominal group resolved?
+			if ng.id:
+				logging.info("\tAssign to ng: " + colored_print(ng.id, 'white', 'blue'))
+				ng._resolved = True
+				
+			resolved = resolved and ng._resolved
+			
+		return [ngs, resolved]
+	
+	
+	builder = NominalGroupStatementBuilder(None, current_speaker)
+		
+	if sentence.sn:
+		res_sn = resolve_ng(sentence.sn, builder)
+		sentence.sn = res_sn[0]
+		
+	
+	if sentence.sv:
+		for sv in sentence.sv:
+			sv._resolved = True
+
+			if sv.d_obj:
+				res_d_obj = resolve_ng(sv.d_obj, builder)
+				sv.d_obj = res_d_obj[0]
+				sv._resolved = sv._resolved and res_d_obj[1]
+				
+			if sv.i_cmpl:
+				for i_cmpl in sv.i_cmpl:
+					res_i_cmpl = resolve_ng(i_cmpl.nominal_group, builder)					
+					i_cmpl = res_i_cmpl[0]
+					sv._resolved = sv._resolved and res_i_cmpl[1]
+	
+	print(sentence)
+	print "Sentence resolved ... " , sentence.resolved()
+	
 	return sentence
+
+
 
 
 def unit_tests():
