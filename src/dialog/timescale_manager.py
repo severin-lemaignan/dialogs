41fa5b992a5aa062eb4be86c139c3b70e0ee282c
Mahdi Chouayakh <mchouaya@laas.fr>
2010-08-23 15:01:45 +0200

    start the clock

--- src/dialog/timescale_manager.py
+++ src/dialog/timescale_manager.py
@@ -1,1 +1,15 @@
-TODO
+
+"""
+ Created by Chouayakh Mahdi                                                       
+ 23/08/2010                                                                       
+ The package contains functions that                                         
+ Functions:                                                                       
+                      
+"""
+
+
+def refine_val():
+    while x>valeur:
+        y=x/valeur
+        x=x%valeur
+        
