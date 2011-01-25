9584f726ee04851cbbd8f34436c30886978c6825
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-01-25 19:25:39 +0100

    Renamed module 'dialog' to 'dialogs' to avoid confusion with the (obscure) 'dialog' linux tool

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
- #!/usr/bin/python
+ #!/usr/bin/env python
  # -*- coding: utf-8 -*-
 
 import os
 from distutils.core import setup
 
-setup(name='Dialog',
+setup(name='Dialogs',
       version='0.4',
       license='BSD',
       description='Handles natural language inputs and outputs on cognitive robots',
-      long_description='Dialog parses natural language and tries to ground it with respect to an ontology that is maintained by the robot',
+      long_description='Dialogs parses natural language and tries to ground it with respect to an ontology maintained by the robot',
       author='OpenRobots team',
       author_email='openrobots@laas.fr',
       url='http://softs.laas.fr/openrobots',
       package_dir = {'': 'src'},
-      packages=['dialog', 'dialog.interpretation', 'dialog.parsing', 'dialog.verbalization'],
-      scripts=['scripts/dialog', 'scripts/dialog_test'],
-      data_files=[('share/dialog', ['share/dialog/' + f for f in os.listdir('share/dialog')]),
-                  ('share/doc/dialog', ['AUTHORS', 'LICENSE', 'TODO', 'README']),
-                  ('share/doc/dialog', ['doc/' + f for f in os.listdir('doc')]),
-                  ('share/examples/dialog', ['share/examples/dialog/' + f for f in os.listdir('share/examples/dialog')])]
+      packages=['dialogs', 'dialogs.interpretation', 'dialogs.parsing', 'dialogs.verbalization'],
+      scripts=['scripts/dialogs', 'scripts/dialogs_test'],
+      data_files=[('share/dialogs', ['share/dialogs/' + f for f in os.listdir('share/dialogs')]),
+                  ('share/doc/dialogs', ['AUTHORS', 'LICENSE', 'TODO', 'README']),
+                  ('share/doc/dialogs', ['doc/' + f for f in os.listdir('doc')]),
+                  ('share/examples/dialogs', ['share/examples/dialogs/' + f for f in os.listdir('share/examples/dialogs')])]
       )
