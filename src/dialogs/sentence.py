severin-lemaignan/dialogs

commit e207c234737ee892f30ab7d0a4423e90ac8b8c01

Code refactoring

src/dialogs/sentence.py
@@ -78,7 +78,7 @@ class Sentence:
                 map(lambda x: x.flatten(), self.sn),
                 map(lambda x: x.flatten(), self.sv)]
     
-    def quit_loop(self):
+    def isaborting(self):
         #Forget it
         if self.data_type == "imperative" \
             and "forget" in [verb for sv in self.sv for verb in sv.vrb_main]\
@@ -93,7 +93,7 @@ class Sentence:
             
         return False
     
-    def learn_it(self):
+    def islearning(self):
         #Learn that
         if  self.data_type == "imperative"\
             and "learn" in [verb for sv in self.sv for verb in sv.vrb_main]\
@@ -346,19 +346,17 @@ class Comparator():
     def compare(self, obj1, obj2):
         return obj1.__class__ == obj2.__class__ and \
                 obj1.flatten() == obj2.flatten()
-      
-        
 
-def it_is_pronoun(word):
+def ispronoun(word):
     if word in ResourcePool().pronouns:
-        return 1
-    return 0
-                    
-                           
+        return True
+    return False
+
 def concat_gn(nominal_group_structure, new_class, flag):      
     """
-    concatenates 2 nominal groups                                      
-    Input=2 nominal groups and the flag           Output= nominal group                   
+    concatenates 2 nominal groups
+    Input=2 nominal groups and the flag
+    Output= nominal group
     """
     
     #If failure we need to change information else we add 
@@ -392,8 +390,9 @@ def concat_gn(nominal_group_structure, new_class, flag):
 
 def process_verbal_group_part(verbal_group,nominal_group_structure, flag):  
     """
-    process merge in the verbal part                                      
-    Input=nominal groups, the verbal part and the flag      Output= nominal group                   
+    process merge in the verbal part
+    Input=nominal groups, the verbal part and the flag
+    Output= nominal group
     """
     
     #The direct complement is a nominal group
@@ -424,8 +423,9 @@ def process_verbal_group_part(verbal_group,nominal_group_structure, flag):
 
 def process_verbal_group_nega_part(verbal_group,nominal_group_structure, flag):  
     """
-    process merge in the verbal part when we have negative sentence                                    
-    Input=nominal groups, the verbal part and the flag      Output= nominal group                   
+    process merge in the verbal part when we have negative sentence
+    Input=nominal groups, the verbal part and the flag
+    Output= nominal group
     """
     
     #The direct complement is a nominal group
@@ -468,8 +468,9 @@ def process_verbal_group_nega_part(verbal_group,nominal_group_structure, flag):
 
 def refine_nominal_group_relative_sv (verbal_structure,nominal_group):
     """
-    replaces one by the noun in verbal part of relative                                      
-    Input=nominal groups and verbal part          Output= nominal group                   
+    replaces one by the noun in verbal part of relative
+    Input=nominal groups and verbal part
+    Output= nominal group
     """
     
     #For direct complement
@@ -492,8 +493,9 @@ def refine_nominal_group_relative_sv (verbal_structure,nominal_group):
 
 def refine_nominal_group_relative(nominal_group):
     """
-    replaces one by the noun in relative                                      
-    Input=nominal groups                      Output= nominal group                   
+    replaces one by the noun in relative
+    Input=nominal groups
+    Output= nominal group
     """
     
     for i in nominal_group.relative:
@@ -503,66 +505,64 @@ def refine_nominal_group_relative(nominal_group):
             refine_nominal_group_relative(ns)
         for verbal_structure in i.sv:
             refine_nominal_group_relative_sv(verbal_structure,nominal_group)
-            
-            
+
 def i_cmpl(indirect_complement):
     """
-    separates indirect complements when they have same preposition                                     
-    Input=indirect complement                  Output=indirect complement                   
+    separates indirect complements when they have same preposition
+    Input=indirect complement
+    Output=indirect complement
     """
-    
+
     #init
     i=0
-    
+
     while i<len(indirect_complement):
         if len(indirect_complement[i].nominal_group)>1:
             list_nominal_group=indirect_complement[i].nominal_group[1:]
             indirect_complement[i].nominal_group=[indirect_complement[i].nominal_group[1]]
             for k in list_nominal_group:
                 indirect_complement=indirect_complement+[Indirect_Complement(indirect_complement[i].prep,[k])]
-        i=i+1  
+        i=i+1
     return indirect_complement
-        
-        
-        
+
 def nominal_group_remerge(utterance, flag , nominal_group_structure):
     """
-    process merge                                      
-    Input=nominal groups, the use utterance and the flag      Output= nominal group                   
+    process merge
+    Input=nominal groups, the use utterance and the flag      Output= nominal group
     """
 
     for i in utterance:
         if i.data_type==Sentence.imperative:
             i.data_type=Sentence.statement
             i.sn=[Nominal_Group(['the'],i.sv[0].vrb_main,[],[],[])]
-      
+
         if i.data_type==Sentence.statement or i.data_type.startswith(Sentence.subsentence) :
             if i.sv[0].state==Verbal_Group.affirmative:
-            
+
                 #We can have just the subject
                 if i.sv[0].d_obj==[] and i.sv[0].i_cmpl==[] and i.sv[0].sv_sec==[] and i.sv[0].vrb_sub_sentence==[]:
                     for k in i.sn:
                         concat_gn(nominal_group_structure, k, flag)
                     refine_nominal_group_relative(nominal_group_structure)
                     return nominal_group_structure
-                
+
                 for k in i.sn:
-                    if it_is_pronoun(k.noun[0])==0:
-                        concat_gn(nominal_group_structure, k, flag)                    
+                    if ispronoun(k.noun[0]):
+                        concat_gn(nominal_group_structure, k, flag)
                 #We finish the process with the verbal part
                 for v in i.sv:
-                    nominal_group_structure=process_verbal_group_part(v,nominal_group_structure, flag)        
-            
+                    nominal_group_structure=process_verbal_group_part(v,nominal_group_structure, flag)
+
             elif i.sv[0].state==Verbal_Group.negative:
                 #For all other sentences flag will be FAILURE
                 flag='FAILURE'
-                
+
                 for k in i.sn:
-                    if it_is_pronoun(k.noun[0])==0:
+                    if ispronoun(k.noun[0]):
                         concat_gn(nominal_group_structure, k, flag)
                 #We finish the process with the verbal par
                 for v in i.sv:
-                    nominal_group_structure=process_verbal_group_nega_part(v,nominal_group_structure, flag)   
-                
+                    nominal_group_structure=process_verbal_group_nega_part(v,nominal_group_structure, flag)
+
     refine_nominal_group_relative(nominal_group_structure)
     return nominal_group_structure

src/dialogs/dialog_core.py
@@ -209,7 +209,7 @@ class Dialog(Thread):
                 
                 break
             
-            if s.learn_it() and self._last_output:
+            if s.islearning() and self._last_output:
                 s.append_sub_sentence(self._last_output['sentence'])
                 self._last_output = None
                 self.waiting_for_more_info = False
@@ -241,7 +241,7 @@ class Dialog(Thread):
             #No more info needed
             self.waiting_for_more_info = False
             
-        #end of Unsifient input or unidentified anaphora Error processing
+        #end of Unsufficient input or unidentified anaphora Error processing
         
         
         #Sentences Queue()
@@ -254,7 +254,7 @@ class Dialog(Thread):
             self._resolver.sentences_store = Dialog.dialog_history
             uie_object = self._last_output['object'] if self._last_output else None
             uie_object_with_more_info = self._last_output['object_with_more_info'] if uie_object else None
-            self._last_output = None  
+            self._last_output = None
             
             uae_object = self._anaphora_input['object'] if self._anaphora_input else None
             uae_object_with_more_info = self._anaphora_input['object_with_more_info'] if uae_object else None
@@ -265,7 +265,7 @@ class Dialog(Thread):
             self._logger.info(colored_print("\n-------[ RESOLVING SENTENCE  ]-------\n", 'green'))
             
             self.active_sentence = self._resolver.references_resolution(self.active_sentence,
-                                                                        self.current_speaker, 
+                                                                        self.current_speaker,
                                                                         uae_object,
                                                                         uae_object_with_more_info,
                                                                         uae_object_list)
@@ -291,14 +291,14 @@ class Dialog(Thread):
             
             if self._demo_mode:
                 wait_for_keypress()
-                
+            
             #Verbalization
             if self._content_analyser.analyse_output():
-                
+            
                 self._logger.info(colored_print("\n-------[  VERBALIZATION   ]-------\n", 'green'))
                 self.last_sentence = (self._content_analyser.analyse_output(), 
                                         self._verbalizer.verbalize(self._content_analyser.analyse_output()))
-                                        
+                
                 self._sentence_output_queue.put(self.last_sentence[0])
                 
                 if self._demo_mode:

src/dialogs/interpretation/resolution.py
@@ -49,7 +49,7 @@ class Resolver:
                                                                                                 uae_object,
                                                                                                 uae_object_with_more_info,
                                                                                                 uae_object_list)
-                                                                                        
+
         logger.info(colored_print("-> Resolving references and anaphors...", 'green'))
         #Record of current sentence
         self._current_sentence = sentence
@@ -231,7 +231,7 @@ class Resolver:
         """This attempts to resolve every single nominal group held in a nominal group list"""
         resolved_sn = []
         for ng in array_sn:
-            if self._current_sentence.learn_it():
+            if self._current_sentence.islearning():
                 ng.noun_cmpl = self._resolve_groups_references(ng.noun_cmpl, matcher, current_speaker, None) if ng.noun_cmpl else []
                 #resolved_relative = [self.references_resolution(relative, current_speaker, None, None, None) for relative in ng.relative]
                 #ng.relative = resolved_relative
@@ -357,7 +357,7 @@ class Resolver:
             id = discriminator.clarify(description, features)
         except UnsufficientInputError as uie:
             #   Create a new concept instead of raising unsificient input error, as the current sentence start with "learn that ..."
-            if self._current_sentence.learn_it():
+            if self._current_sentence.islearning():
                 id = self._ontology_learns_new_concept(stmts, current_speaker)
             else:
                 sf = SentenceFactory()
@@ -409,7 +409,7 @@ class Resolver:
         resolved_sn = []
         for ng in nominal_groups:
             
-            if self._current_sentence.learn_it():
+            if self._current_sentence.islearning():
                 ng.noun_cmpl = self._resolve_groups_nouns(ng.noun_cmpl, current_speaker, discriminator, builder) if ng.noun_cmpl else []
                 #resolved_relative = [self.noun_phrases_resolution(relative, current_speaker, None, None) for relative in ng.relative]
                 #ng.relative = resolved_relative

commit 7de2e3e1f7c2bddd0ab2fe7c540994c07fde7013

Fixing breakage introduced in commit e207c234737e

src/dialogs/sentence.py
@@ -590,7 +590,7 @@ def nominal_group_remerge(utterance, flag , nominal_group_structure):
                     return nominal_group_structure
 
                 for k in i.sn:
-                    if ispronoun(k.noun[0]):
+                    if not ispronoun(k.noun[0]):
                         concat_gn(nominal_group_structure, k, flag)
                 #We finish the process with the verbal part
                 for v in i.sv:
@@ -601,7 +601,7 @@ def nominal_group_remerge(utterance, flag , nominal_group_structure):
                 flag='FAILURE'
 
                 for k in i.sn:
-                    if ispronoun(k.noun[0]):
+                    if not ispronoun(k.noun[0]):
                         concat_gn(nominal_group_structure, k, flag)
                 #We finish the process with the verbal par
                 for v in i.sv: