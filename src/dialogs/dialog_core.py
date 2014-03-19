severin-lemaignan/dialogs

commit 7d63777108e2553db1073794a72a5dc7042aa527

Associate an 'InterrogativeState' to question; delay answer to human
This commit changes the way questions are answered: instead of directly verbalizing the answer to
the interactor, a new interrogative 'state of mind' is created, associated to the human and stored
in the knowledge base, along with the answer the dialog module has computed.

The robot controller is then in charge of monitoring this 'InterrogativeState' and react
appropriately, by telling the answer or maybe triggering other behaviours (for instance, start
lokking for an object if it is not found).

As a side-effect of the commit, the dialogs module won't say anymore 'Ok' or 'Alright' after
an order. These short sentences are now available in the knowledge base as 'sit_id verbalisesTo <text>'

src/dialogs/dialog_core.py
@@ -327,7 +327,7 @@ class Dialog(Thread):
             #Content analysis
             self._logger.info(colored_print("\n-------[   CONTENT ANALYSIS   ]-------\n", 'green'))
             
-            self.last_stmts_set = self._content_analyser.analyse(self.active_sentence, 
+            self.last_stmts_set, self.current_situation_id = self._content_analyser.analyse(self.active_sentence, 
                                                                     self.current_speaker)
             
             if self._demo_mode:
@@ -340,7 +340,14 @@ class Dialog(Thread):
                 self.last_sentence = (self._content_analyser.analyse_output(), 
                                         self._verbalizer.verbalize(self._content_analyser.analyse_output()))
                 
-                self._sentence_output_queue.put(self.last_sentence[0])
+                if self.current_situation_id:
+                    self._logger.debug(colored_print("Answer: <%s>: Sent to knowledge base." % self.last_sentence[1], 'magenta'))
+                    self._logger.debug("Up to the robot controller now!")
+                    ResourcePool().ontology_server.revise(['%s verbalisesTo "%s"' % (self.current_situation_id,
+                                                                                     self.last_sentence[1])],
+                                                        {"method":"add", "models":["myself"]})
+                else:
+                    self._sentence_output_queue.put(self.last_sentence[0])
                 
                 if self._demo_mode:
                     wait_for_keypress()

src/dialogs/interpretation/content_analysis.py
@@ -26,52 +26,61 @@ class ContentAnalyser:
         self.output_sentence = []
         
     def analyse(self, sentence, current_speaker):
-        """analyse sentences"""
+        """Analyse a sentence intent and perform the corresponding behaviour.
+
+        :returns: a pair with
+         - a list of symbolic statements produced
+        during the analyse. Note that these statements have 
+        already been sent to the knowledge base
+         - a situation ID that identifies a situation the speaker is
+         desiring (for an imperative sentence or an order) or 
+         experiencing (like an 'InterrogativeState' for a question).
+
+         These two values can be None.
+
+        Note also that an answer to the speaker is stored in self.output_sentence
+        after analyse() completes.
+
+        It can be used to tell the human to acknowledge an order, answer a 
+        gratulation, the answer a question (or the fact the answer is not known),
+        etc.
+        """
 
         self.builder.clear_all()
         self.output_sentence = []
-        
+
         sentence = self.pre_analyse_content(sentence)
-        
+
         if sentence.data_type == [INTERJECTION, EXCLAMATION]:
             pass
-        
+
         if sentence.data_type in [START, END]:
             self.output_sentence.append(sentence)
-           
+
         if sentence.data_type == GRATULATION:
             self.output_sentence.extend(self.sfactory.create_gratulation_reply())
-        
+
         if sentence.data_type in [AGREEMENT, DISAGREEMENT]:
             self.output_sentence.extend(self.sfactory.create_agree_reply())
-            
+
         if sentence.data_type in [IMPERATIVE, STATEMENT]:
             logger.debug(colored_print("Processing the content of " +  ("an imperative sentence" if sentence.data_type == IMPERATIVE else "a statement "), "magenta"))
             return self.process_sentence(sentence, current_speaker)
-        
+
         if sentence.data_type in [W_QUESTION, YES_NO_QUESTION]:
             logger.debug(colored_print("Processing the content of " +  ("a W question " if sentence.data_type == W_QUESTION else "a YES/NO question"), "magenta"))
             return self.process_question(sentence, current_speaker)
-        
-        
-            
+
+        return None, None # default: no statement generated, no situation ID
+
     def process_sentence(self, sentence, current_speaker):
         self.builder.set_current_speaker(current_speaker)
 
         stmts, situation_id = self.builder.process_sentence(sentence)
-        
+
         if stmts:
-            logger.info("Generated statements: ")
-            for s in stmts:
-                logger.info(">> " + colored_print(s, None, 'magenta'))
-            
-            self.adder._current_speaker = self.builder._current_speaker
-            self.adder._unclarified_ids = self.builder._unclarified_ids
-            self.adder._statements = stmts
-            self.adder._statements_to_remove = self.builder._statements_to_remove
-            stmts = self.adder.process()
-            
-            logger.debug("...added to the ontology")
+
+            self.add_stmts(stmts)
 
             emotions.satisfied()
 
@@ -87,11 +96,20 @@ class ContentAnalyser:
         if self.builder.lear_more_concept:
             self.output_sentence.extend(self.sfactory.create_what_is_a_reference(sentence, self.builder.lear_more_concept))
         
-        return stmts
-    
+        return stmts, situation_id
+
+
     def process_question(self, sentence, current_speaker):
         self.question_handler.set_current_speaker(current_speaker)
-        answer = self.question_handler.process_sentence(sentence)
+
+        # 'stmts' contains a list of statement describing the current 
+        # 'interrogative state' of the interactor.
+        answer, stmts, situation_id = self.question_handler.process_sentence(sentence)
+
+        if stmts:
+            self.add_stmts(stmts)
+        else:
+            logger.info("No statements produced")
         
         if answer:
             emotions.satisfied()
@@ -109,18 +127,32 @@ class ContentAnalyser:
         if sentence.data_type == YES_NO_QUESTION:
             self.output_sentence.extend(self.sfactory.create_yes_no_answer(sentence, answer))
         
-        return self.question_handler._statements
+        return stmts, situation_id
 
+    
+    def add_stmts(self, stmts):
+        logger.info("Generated statements: ")
+        for s in stmts:
+            logger.info(">> " + colored_print(s, None, 'magenta'))
+        
+        self.adder._current_speaker = self.builder._current_speaker
+        self.adder._unclarified_ids = self.builder._unclarified_ids
+        self.adder._statements = stmts
+        self.adder._statements_to_remove = self.builder._statements_to_remove
+        stmts = self.adder.process()
+        
+        logger.debug("...added to the ontology")
 
     def analyse_output(self):
         return self.output_sentence
         
     def pre_analyse_content(self, sentence):
-        """ this method analyse the content of a sentence ang give it another processing purpose.
-            E.g: Can you give me the bottle?
-            The sentence above is of YES_NO_QUESTION type but should actually be processed as an order in which the current speaker
-            desires 'the bottle'. 
-            Therefore, we turn it into 'give me the bottle'.
+        """ this method analyse the content of a sentence and possibly changes its purpose.
+
+        E.g: Can you give me the bottle?
+        The sentence above is of YES_NO_QUESTION type but should actually
+        be processed as an order in which the current speaker desires 'the
+        bottle'. Therefore, we turn it into 'give me the bottle'.
         """
         # Case of : 
         #   -INPUT:  Yes_no_question + can + action verb

src/dialogs/interpretation/questions_handler.py
@@ -37,7 +37,7 @@ class QuestionHandler:
         # sentence.
         self.process_on_knowing_concept = False
 
-        # This defined the default model used to resolve queries.
+        # This defines the default model used to resolve queries.
         self._default_agent = "myself"
 
     def clear_statements(self):
@@ -96,11 +96,11 @@ class QuestionHandler:
             self._query_on_field = self._set_query_on_field(sentence)
             statements_with_bound_tokens =  self._remove_statements_with_no_unbound_tokens(self._statements)
             
-            #Agent from whith the ontology model is queried
+            #Agent from which the ontology model is queried
             agent = self._default_agent
             
             self._answer = []
-            
+
             #For all the possible subjects of a same question
             if not self._sentence.sn:
                 statements_to_query = [self._extend_statement_from_sentence_aim(statements_with_bound_tokens)]
@@ -178,8 +178,22 @@ class QuestionHandler:
                     pass
 
                 self._statements.extend(statements)
-        return self._answer
-        
+
+        stmts, sit_id = self._build_situation_statements()
+        return self._answer, stmts, sit_id
+    
+    def _build_situation_statements(self):
+        sit_id = generate_id(with_question_mark = False)
+
+        stmts = ["%s experiences %s" % (self._current_speaker, sit_id)]
+        stmts += ["%s rdf:type InterrogativeState" % sit_id]
+        for sn in self._sentence.sn:
+            stmts += ["%s hasObject %s" % (sit_id, sn.id)]
+        stmts += ["%s hasAim %s_question_aim" % (sit_id, self._sentence.aim)]
+        stmts += ["%s hasAnswer %s" % (sit_id, "true" if self._answer else "false")]
+
+        return stmts, sit_id
+    
     def _set_situation_id(self, statements):
         """This attempts to clarify the ID of an action verbs
             E.g: statement = [?event rdf:type Go, ?event performedBy myself, ?event actsOnObject xxx]

commit 2678583f6286b13df14c492d6897665fb13aa3d0

Make sure Dialogs answer are stored in the history
Fix bug introduced by commit 7d63777108e2553db1

src/dialogs/dialog_core.py
@@ -174,9 +174,7 @@ class Dialog(Thread):
                             nl_output, \
                             'blue') + "\n")
                             
-                # Store output 
-                Dialog.dialog_history.extend(output)
-                
+               
                 
             except Empty:
                 pass
@@ -371,8 +369,10 @@ class Dialog(Thread):
                 self.last_sentence = (None, "")
             
             #Dialog History
-            self._logger.debug(colored_print("Sentence saved in history.", 'magenta'))
+            self._logger.debug(colored_print("Sentence and answer saved in history.", 'magenta'))
             Dialog.dialog_history.append(self.active_sentence)
+            if self.last_sentence[0]:
+                Dialog.dialog_history.extend(self.last_sentence[0])
             
         #Finalizing the processing
         self.active_sentence = None